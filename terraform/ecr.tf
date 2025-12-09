058b33ac7c71ce8554cea397cbe677f292db7df1
SzekelyBoti <szekelyboti1@gmail.com>
2025-12-09 16:46:56 +0100

    CI/CD tweaks 15

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -9,7 +9,3 @@ resource "aws_ecr_repository" "backend" {
 resource "aws_ecr_repository" "populate" {
   name = "employee-madness-populate"
 }
-
-output "ecr_frontend_uri" { value = aws_ecr_repository.frontend.repository_url }
-output "ecr_backend_uri"  { value = aws_ecr_repository.backend.repository_url }
-output "ecr_populate_uri" { value = aws_ecr_repository.populate.repository_url }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@
 output "eks_kubeconfig_command" {
   value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.my_cluster.name}"
 }
+
+output "ecr_frontend_uri" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "ecr_backend_uri" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "ecr_populate_uri" {
+  value = aws_ecr_repository.populate.repository_url
+}
